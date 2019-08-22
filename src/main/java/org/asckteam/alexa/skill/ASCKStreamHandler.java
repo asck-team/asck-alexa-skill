0a3848e33ea02d5bb790ad09d97010401a9ff3d0
Constantin Krüger <Constantinidis@web.de>
2019-08-23 00:53:34 +0200

    add requesthandlers and skillid

--- src/main/java/org/asckteam/alexa/skill/ASCKStreamHandler.java
+++ src/main/java/org/asckteam/alexa/skill/ASCKStreamHandler.java
@@ -3,13 +3,14 @@ package org.asckteam.alexa.skill;
 import com.amazon.ask.Skill;
 import com.amazon.ask.SkillStreamHandler;
 import com.amazon.ask.Skills;
+import org.asckteam.alexa.skill.handlers.*;
 
 @SuppressWarnings("unused")
 public class ASCKStreamHandler extends SkillStreamHandler {
 
 
     private static Skill getSkill() {
-        return Skills.standard().addRequestHandlers().build();
+        return Skills.standard().addRequestHandlers(new ASCKIntentHandler(), new CancelAndStopIntentHandler(), new FallbackIntentHandler(), new HelpIntentHandler(), new LaunchRequestHandler(), new SessionEndedRequestHandler()).withSkillId("amzn1.ask.skill.8093cb49-747c-4553-817f-a7bd32da063e").build();
     }
 
     public ASCKStreamHandler() {
